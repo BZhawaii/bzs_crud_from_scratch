11d0b9e5abb0c13542d3de5bd5bafbc2f76515e4
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2017-02-21 20:55:38 -0700

    Added add author with a redirect to authors

--- routes/authors.js
+++ routes/authors.js
@@ -5,12 +5,11 @@ var knex = require('../db/knex');
 
 // GET authors listing.
 router.get('/', function(req, res) {
-  console.log("Hello, we're in books.js");
+  console.log("Hello, we're in authors.js");
   knex('authors')
     .select('id','first_name','last_name', 'portrait', 'bio')
     .orderBy('last_name', 'asc')
     .then(function(ans) {
-      console.log("HERE ",ans);
       res.render("authors", {
         data: ans
       });
@@ -21,6 +20,36 @@ router.get('/', function(req, res) {
 router.get('/add', function(req, res, next) {
   res.render('add-author');
 }); //this closes GO TO new author form.
+
+function validForm(author) {
+  return typeof author.first_name == 'string' &&
+          typeof author.last_name == 'string' &&
+          typeof author.bio == 'string';
+}; //closes validForm
+
+// CREATE a new author in database.
+router.post('/', function(req, res) {
+  console.log("This is REQ.BODY ", req.body);
+  if(validForm(req.body)) { //launches validForm function
+    var authorObj = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      portrait: req.body.portrait,
+      bio: req.body.bio
+    }; //closes obj
+    knex('authors')
+      .insert(authorObj, 'id')
+      .then(function(ans) {
+        var id = ans[0];
+        res.redirect(`authors`);
+      }) //closes then
+  } else {
+    res.status(500);
+    res.render('error', {
+      message: 'Invalid author'
+    }); //closes res.render
+  }; //closes if else
+}); //this closes CREATE a new author in database.
 
 //GO TO single author page
 router.get('/:id', function(req, res) {
